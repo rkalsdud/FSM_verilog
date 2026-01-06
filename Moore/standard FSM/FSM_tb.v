8786cbbebb181584bdfa444623638b62dedb0e39
minyeong ka <rkalsdud0707@gmail.com>
2026-01-06 11:10:14 +0900

    Add files via upload

---
+++ Moore/standard FSM/FSM_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+module FSM_tb();
+
+wire [2:0] state;
+wire z;
+reg clk, x, reset;
+
+initial begin
+    reset=1'b0;
+    clk=1'b0;
+    x=1'b0;
+end
+
+mooreFSM mod(
+    .x(x), .reset(reset), .clk(clk),
+    .z(z), .state(state)
+);
+
+always #10 clk=~clk;
+always #23 x=~x;
+initial #400 reset=~reset;
+
+initial begin
+    #1000
+    $finish;
+end
+
+endmodule
---
+++ Moore/standard FSM/mooreFSM.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+module mooreFSM(
+input x, clk, reset,
+output reg [2:0] state,
+output z
+);
+    
+reg [2:0] nextState;    
+localparam S0=3'b000, S1=3'b001, S2=3'b010, S3=3'b011, S4=3'b100, S5=3'b101;
+
+always @(*) begin
+    case (state)
+        S0: nextState=x ? S1:S0;
+        S1: nextState=x ? S1:S2;
+        S2: nextState=x ? S3:S0;
+        S3: nextState=x ? S1:S4;
+        S4: nextState=x ? S5:S0;
+        S5: nextState=x ? S1:S4;
+        default: nextState= S0;
+    endcase
+end    
+    
+always @(posedge clk or negedge reset) begin
+    if (!reset) state<=S0;
+    else state<=nextState;
+end
+
+assign z= (state==S5);
+
+endmodule
